cf75dab8022d7dc7b6ccd8a1964603979c1c8b57
John Watts <johnwatts@gmx.com>
2017-03-03 11:33:59 -0600

    JavaScript updated with all buttons flashing in a random array

--- public/simple_simon.js
+++ public/simple_simon.js
@@ -1,48 +1,74 @@
 "use strict";
-//Game sequence
+
 var sequence = [];
-//User input sequence
-var userInput = [];
+var level = 1;
+var elem = '.button';
 
-//change button opacity on click and then reset after 1/2 second
-$('.button').click(function() {
-  $(this).css('opacity', '0.6');
-  setTimeout(function(){
-  	$('.button').css('opacity', '1');
-	}, 200);
-});
+var generateRandomArray = function(elem, max) {
 
-
-// Generate a random square
-
-function getRandomNumberBetweenOAnd (input) {
-	return Math.floor(Math.random() * input);
+var randomIndex = Math.floor(Math.random() * 4) + 1;
+sequence.push(randomIndex);
 }
 
-var squares =["red", "yellow", "green", "blue"];
+//Change opacity of buttons to flash during random sequence
+var flash = function(elem) {
+$(elem).css('opacity', '0.4');
+setTimeout(function(){
+$(elem).css('opacity', '1');
+}, 500);
+}
 
-var randomIndex = getRandomNumberBetweenOAnd(squares.length);
+//Play sequence function(color)
+var playSequence = function(seq, elem) {
+var i=0;
+	var intervalId = setInterval(function() {
+		switch(seq[i]){
+			case 1 :
+				flash("#red");
+				break;
+			case 2 :
+				flash("#blue");
+				break;
+			case 3 : 
+				flash("#yellow");
+				break;
+			case 4 :
+				flash("#green");
+				break;
+		}
+		if(i < seq.length) {
+			i++;
+		} else {
+			clearInterval(intervalId);
+		}
 
-var randomSquare = squares[randomIndex];
+	},1000)
+}
+//Function to start game 
+	//It's going to call next round function
+var init = function(lev) {
+$('#level').text('Level: ' + lev);
 
-console.log(randomSquare);
+generateRandomArray(elem, lev);
+playSequence(sequence, elem);
+console.log(sequence)
+}
+
 
 //event to fire start game function when start button clicked then deactivate start game button
  
 $("#start").click(function() {
-  sequence = [];
-  userInput = [];
   $("#start").hide();
   $("#restart").show();
-  start();
+  init(1);
 });
 
-//
-//Function to start game 
-	//It's going to call next round function
-function start() {
-  playSequence(); 
-}
+//Restart function 
+$("#restart").click(function() {
+  $("#start").show();
+  $("#restart").hide();
+});
+
 
 //Next round function
 	//add a random string to the sequence
