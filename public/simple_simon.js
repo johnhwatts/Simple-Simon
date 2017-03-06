ad31386c730e72056320808859907036ec220b82
John Watts <johnwatts@gmx.com>
2017-03-06 13:40:14 -0600

    add score functions to JS and update HTML and CSS to reflect level counter and score counter and next level button

--- public/simple_simon.js
+++ public/simple_simon.js
@@ -5,10 +5,21 @@ var userInput = [];
 var level = 1;
 var clickCounter = 0;
 var elem = '.button';
+var disableControls = [];
 
-var getScore = function() {
+//functions
 
-}
+
+//add to score if user scores point - increase by num
+function score(num) {
+currentScore = parseInt(currentScore + num);
+$('#score').text('Score: ' + currentScore);
+};
+
+//get the current score
+function getScore() {
+return currentScore;
+};
 
 var generateRandomArray = function(elem, max) {
 
@@ -50,15 +61,42 @@ var i=0;
 
 	},1000)
 }
+
 //Function to start game 
-	//It's going to call next round function
+	//It's going to call next level function
 var init = function(lev) {
 $('#level').text('Level: ' + lev);
 
 generateRandomArray(elem, lev);
 playSequence(sequence, elem);
-console.log(sequence)
-}
+console.log(sequence);
+};
+
+//apply click events to buttons
+var applyClicks = function(elem) {
+	$(elem).each(function(i) {
+	$(this).click(function() {
+	// if (!disableControls) {
+	// 	flash($(this));
+	// 	userInput.push(i);
+	if (userInput[clickCounter] == sequence[clickCounter]) { //if click is correct
+		score(1);
+	if (userInput.length == sequence.length) { //if user has completed clicks for this round
+		disableControls = true;
+		currentLevel++;
+	$('#next').show(); //show next level button
+	}
+	}
+	else { //if click is false - RESET GAME
+		alert('You failed, sucker!!! Final Score: ' + getScore());
+		resetScore();
+		resetLevel();
+		disableControls = true;
+	}
+	clickCounter++;
+	});
+	});
+};
 
 
 //event to fire start game function when start button clicked then deactivate start game button
@@ -73,26 +111,24 @@ $('#start').click(function() {
 $('#restart').click(function() {
   $('#start').show();
   $('#restart').hide();
-  // resetScore();
-  // resetLevel();
+  resetScore();
+  resetLevel();
   init(1);
 });
 
+applyClicks('.button'); // apply logic on buttons
 
 //Next level function
-	$('#next').click(function() {
-	init(currentLevel);
+$('#next').click(function() {
+init(currentLevel);
 });
 
-//Play sequence function(color)
-	// check the color then animate the correct color
-	// switch(color)
-		//case "red":
-			//change opacity for the red button
-
-
+//reset score if user fails
+resetScore = function() {
+jQuery('#score').text('Score: 0');
+};
 
 
 //Game over function to reset game and reactivate start game button
 
-
+	
